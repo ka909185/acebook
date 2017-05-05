7ea3565d674e87e668f03e84387c5de405be0107
ka909185 <kaku.ogi@gmail.com>
2017-05-06 02:11:23 +0900

    メール認証機能

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -19,6 +19,7 @@ class TopicsController < ApplicationController
     @topic.user_id = current_user.id
     if @topic.save
       redirect_to topics_path, notice: "トピックスを作成しました！"
+      NoticeMailer.sendmail_topic(@topic).deliver
     else
       render 'new'
     end
